15152fbb3dbf62acd77a8f1a1d3bfe6d2f3021e1
TheDingodile <jakob@3esser.dk>
2023-01-03 23:18:47 +0100

    day1done

--- s1_development_environment/exercise_files/final_exercise/data.py
+++ s1_development_environment/exercise_files/final_exercise/data.py
@@ -1,13 +1,25 @@
 import numpy as np
-import matplotlib as plt
+from os import listdir
+import os
+import helper
+import torch
+import matplotlib.pyplot as plt
 
 def mnist():
     # Define a transform to normalize the data
-    d = np.load("data/corruptmnist/train_2.npz")
-    lst = d.files
-    for item in lst:
-        print(item)
-        print(d[item].shape)
-    return train, test
+    files = listdir("data/corruptmnist")
+    train_in = np.empty(shape=(0, 28, 28))
+    train_out = np.empty(shape=(0))
+
+    test_in = np.empty(shape=(0, 28, 28))
+    test_out = np.empty(shape=(0))
+    for f in files:
+        if f[0:5] == "train":
+            train_in = np.concatenate((train_in, np.load("data/corruptmnist/" + f)['images']), axis=0)
+            train_out = np.concatenate((train_out, np.load("data/corruptmnist/" + f)['labels']), axis=0)
+        else:
+            test_in = np.concatenate((test_in, np.load("data/corruptmnist/" + f)['images']), axis=0)
+            test_out = np.concatenate((test_out, np.load("data/corruptmnist/" + f)['labels']), axis=0)
+    return (train_in, train_out), (test_in, test_out)
 
 mnist()
--- s1_development_environment/exercise_files/final_exercise/main.py
+++ s1_development_environment/exercise_files/final_exercise/main.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import matplotlib.pyplot as plt
 
 import torch
 import click
@@ -22,6 +23,29 @@ def train(lr):
     # TODO: Implement training loop here
     model = MyAwesomeModel()
     train_set, _ = mnist()
+    input, labels = torch.tensor(train_set[0]), torch.tensor(train_set[1])
+    all_loss = []
+    for epoch in range(100):
+        running_loss = 0
+        for i in range(len(input)//50):
+            images = input[50 * i: 50 * (i + 1)]
+            labs = labels[50 * i: 50 * (i + 1)]
+            # Flatten MNIST images into a 784 long vector
+            images = images.view(images.shape[0], -1)
+            # TODO: Training pass
+            output = model.forward(images)
+            loss = model.criterion(output, labs.to(torch.long))
+            loss.backward()
+            model.optimizer.step()
+            model.optimizer.zero_grad()
+            
+            running_loss += loss.item()
+        else:
+            print(f"Training loss: {running_loss / i}")
+            all_loss.append(running_loss)
+    plt.plot(all_loss)
+    plt.show()
+    torch.save(model, "trained_model.pt")
 
 
 @click.command()
@@ -33,6 +57,10 @@ def evaluate(model_checkpoint):
     # TODO: Implement evaluation logic here
     model = torch.load(model_checkpoint)
     _, test_set = mnist()
+    input, labels = torch.tensor(test_set[0]), torch.tensor(test_set[1])
+    images = input.view(input.shape[0], -1)
+    output = torch.argmax(model.forward(images), dim=1)
+    print("accuracy is " + str((5000 - torch.count_nonzero(output - labels).item())/(5000) * 100) + " %")
 
 
 cli.add_command(train)
